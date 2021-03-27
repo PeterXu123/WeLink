57a4372edeaea19c14dd5932e0c6b1f005c9e243
peterx <xu.kang@husky.neu.edu>
2021-03-27 18:16:58 -0400

    moved main activity to login_signup package

--- app/src/main/java/edu/neu/madcourse/welink/login_signup/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/welink/login_signup/MainActivity.java
@@ -1,8 +1,10 @@
-package edu.neu.madcourse.welink;
+package edu.neu.madcourse.welink.login_signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import edu.neu.madcourse.welink.R;
 
 public class MainActivity extends AppCompatActivity {
 
