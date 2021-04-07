72a75ad93327c1c1c28d6d791c771d52f3c4f0df
peterx <xu.kang@husky.neu.edu>
2021-04-06 20:05:39 -0400

    added toast message to search friend result if no user is found

--- app/src/main/java/edu/neu/madcourse/welink/following/search/SearchResultAdapter.java
+++ app/src/main/java/edu/neu/madcourse/welink/following/search/SearchResultAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -92,6 +93,9 @@ public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultHolder
 
                             }
                         });
+                    }
+                    else {
+                        Toast.makeText(context, "no users found!", Toast.LENGTH_LONG).show();
                     }
 
                 }
