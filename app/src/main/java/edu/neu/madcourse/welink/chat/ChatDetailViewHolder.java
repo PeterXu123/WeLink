a34a08a5c9b349fa4bd8fc9b3d8d607e9c5b009e
WeihanLiu <liu.weih@husky.neu.edu>
2021-04-16 16:03:50 -0400

    merge conflict

--- app/src/main/java/edu/neu/madcourse/welink/chat/ChatDetailViewHolder.java
+++ app/src/main/java/edu/neu/madcourse/welink/chat/ChatDetailViewHolder.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package edu.neu.madcourse.welink.chat;
 
 import android.view.View;
@@ -18,25 +17,3 @@ public class ChatDetailViewHolder extends RecyclerView.ViewHolder {
         this.message = itemView.findViewById(R.id.message_content);
     }
 }
-=======
-package edu.neu.madcourse.welink.chat;
-
-
-import android.view.View;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import edu.neu.madcourse.welink.R;
-
-public class ChatDetailViewHolder extends RecyclerView.ViewHolder {
-    TextView sender;
-    TextView message;
-    public ChatDetailViewHolder(@NonNull View itemView) {
-        super(itemView);
-        this.sender = itemView.findViewById(R.id.message_sender);
-        this.message = itemView.findViewById(R.id.message_content);
-    }
-}
->>>>>>> Stashed changes
