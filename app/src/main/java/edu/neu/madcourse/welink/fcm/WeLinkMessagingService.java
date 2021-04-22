4f7975812dadbf29b8e5d387aedc207038c01308
peterx <xu.kang@husky.neu.edu>
2021-04-21 23:31:19 -0400

    added notification

---
+++ app/src/main/java/edu/neu/madcourse/welink/fcm/WeLinkMessagingService.java
@@ -0,0 +1,76 @@
+package edu.neu.madcourse.welink.fcm;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import edu.neu.madcourse.welink.R;
+import edu.neu.madcourse.welink.login_signup.MainActivity;
+
+public class WeLinkMessagingService extends FirebaseMessagingService {
+    private static final String CHANNEL_ID  = "CHANNEL_ID";
+    private static final String CHANNEL_NAME  = "CHANNEL_NAME";
+    private static final String CHANNEL_DESCRIPTION  = "CHANNEL_DESCRIPTION";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+        if (remoteMessage.getNotification() != null) {
+            showNotification(remoteMessage);
+        }
+//        extractPayloadDataForegroundCase(remoteMessage);
+
+    }
+    private void showNotification(RemoteMessage remoteMessage) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification notification;
+        NotificationCompat.Builder builder;
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_HIGH);
+            // Configure the notification channel
+            notificationChannel.setDescription(CHANNEL_DESCRIPTION);
+            notificationManager.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+
+        }
+        else {
+            builder = new NotificationCompat.Builder(this);
+        }
+        notification = builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notificationManager.notify(0,notification);
+
+    }
+}
+
+
+
