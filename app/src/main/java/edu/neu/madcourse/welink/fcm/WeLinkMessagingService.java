077a113fa1357f6f83029c32bbf4cdaa90b91e82
peterx <xu.kang@husky.neu.edu>
2021-04-22 11:59:52 -0400

    disallow navigation with notification if the app is in foreground

--- app/src/main/java/edu/neu/madcourse/welink/fcm/WeLinkMessagingService.java
+++ app/src/main/java/edu/neu/madcourse/welink/fcm/WeLinkMessagingService.java
@@ -1,15 +1,20 @@
 package edu.neu.madcourse.welink.fcm;
 
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.List;
+import java.util.Random;
 
 import edu.neu.madcourse.welink.R;
 import edu.neu.madcourse.welink.fragmentActivity.FragmentActivity;
@@ -31,13 +36,30 @@ public class WeLinkMessagingService extends FirebaseMessagingService {
         // and data payloads are treated as notification messages. The Firebase console always sends notification
         // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
         // [END_EXCLUDE]
-        if (remoteMessage.getNotification() != null) {
-            showNotification(remoteMessage);
+        if (remoteMessage.getNotification() != null && !applicationInForeground()) {
+
+            showNotification(remoteMessage, false);
+        }
+        else {
+            showNotification(remoteMessage, true);
         }
 //        extractPayloadDataForegroundCase(remoteMessage);
 
     }
-    private void showNotification(RemoteMessage remoteMessage) {
+
+    private boolean applicationInForeground() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> services = activityManager.getRunningAppProcesses();
+        boolean isActivityFound = false;
+
+        if (services.get(0).processName
+                .equalsIgnoreCase(getPackageName()) && services.get(0).importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+            isActivityFound = true;
+        }
+
+        return isActivityFound;
+    }
+    private void showNotification(RemoteMessage remoteMessage, boolean isForeground) {
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -62,15 +84,26 @@ public class WeLinkMessagingService extends FirebaseMessagingService {
         else {
             builder = new NotificationCompat.Builder(this);
         }
-        notification = builder.setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .build();
+        if (isForeground){
+            notification = builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true)
+                    .build();
+        }
+        else {
+            notification = builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                    .setContentText(remoteMessage.getNotification().getBody())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .build();
+        }
+
         notificationManager.notify(0,notification);
 
     }
+
 }
 
 
